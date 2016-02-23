628205257ab95008e09e7554c93dfd4cfa7e0dbb
bluepangolin55 <bluepangolin55@gmail.com>
2016-02-23 16:36:55 +0100

    added comments

--- command line hangman/src/Hangman.java
+++ command line hangman/src/Hangman.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 
 public class Hangman {
 
-// ---- main method ----
+	/** This is the main method of our program. When the program starts, only
+	 * this method is invoked. It is static because it is not attached 
+	 * to an instance of the class Hangman. 
+	 */
 	public static void main(String[] args) {
+		// create a new instance of our game with the parameter "Programming"
 		Hangman game = new Hangman("Programming");
+		// run the method start() on our newly initialised game
 		game.start();
 	}
 	
+
 // ---- fields ----	
 	
 	/** The word to be guessed as an array of characters*/
@@ -16,49 +22,75 @@ public class Hangman {
 	/** An array that indicates which character is visible (has been guessed)*/
 	private boolean visible[];
 
-	/** An array that indicates which character is visible (has been guessed)*/
+	/** The amount of lives a player has */
 	private int lives;
-	private boolean correct;
+
+	/** Is true if all characters have been guessed and false otherwise */
+	private boolean solved;
+	
+	/** A helper object that reads the user input from the console */
 	private Scanner in;
+	
+	
 // ---- methods ----
 	
-	// -- constructor -- 
+	/**  Constructor - This is what gets executed when we create an object of this class. 
+	 * 		Here "this" is used because we have two variables called "word". 
+	 * 		"word" refers to the method argument and "this.word" refers
+	 * 		to the field of the object. 
+	 * 	In our constructor we generally want to initialise the object. 
+	 *  Often this means initialising the fields of the object. */
 	public Hangman(String word){
 		this.word = word.toCharArray();
-		this.visible = new boolean[word.length()];
-		this.lives = 5;
-		this.correct = false;
-		this.in = new Scanner(System.in);
-
+		visible = new boolean[word.length()];
+		lives = 5;
+		solved = false;
+		in = new Scanner(System.in);
 	}
 	
 	public void start(){
 		System.out.println("Hi, welcome to Hangman");
 		System.out.println("Try to guess the word!");
 
-		// game loop
-		while(lives > 0 && !correct){
+		// main game loop - we run this as long as the game is active
+		while(lives > 0 && !solved){
 			System.out.println("Guess a character!");
-			char input = in.next().charAt(0);
+			char input = in.next().charAt(0); // reads the user input
+			boolean correctGuess = false; // whether the user guessed correctly this turn
 
-			// print the guessed characters
+			int numberOfGuessedChars = 0; // the number of already guessed characters
 			for(int i=0; i<word.length; i++){
 				if(word[i] == input){
 					visible[i] = true;
+					correctGuess = true;
 				}
 				if(visible[i]){
 					System.out.print(word[i]);
+					numberOfGuessedChars ++;
 				}
 				else{
 					System.out.print('_');
 				}
 				System.out.print(' ');
 			}
-			System.out.println("");
+			System.out.println(""); // print a new line
 
+			if(numberOfGuessedChars == word.length){
+				solved = true;
+			}
+			
+			if(!correctGuess){
+				lives --;
+			}
+		}
+		
+		if(solved){
+				System.out.println("Great, you won the game!");
+		}
+		else{
+				System.out.println("You have used up all your lives :(");
 		}
 
 	}
-
 
 }
